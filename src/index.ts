e8705f53dbd963420f1fa83aac14e35e91ddb2a3
namqh <qhnam.67@gmail.com>
2024-05-07 16:10:32 +0700

    add body parser

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("OK la");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json(req.body);
 });
 
 app.listen(port, () => {
